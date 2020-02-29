24c18026aadc0f7acf86a6d6afd619ffd561b369
Artem Prudnikov <artem.prudnikov57@gmail.com>
2020-02-29 21:54:10 +0300

    added slider

---
+++ dist/assets/js/main.js
@@ -0,0 +1,16 @@
+// $('.instruments').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 4000,
+//     adaptiveHeight: true,
+//     arrows: true
+//   });
+
+$(document).ready(function() {
+  $('.instruments__slider').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true
+  });
+});
