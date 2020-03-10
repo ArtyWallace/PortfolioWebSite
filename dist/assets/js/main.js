ce7c7c51f28cf122c645abb273d0c08562fa9898
Artem Prudnikov <artem.prudnikov57@gmail.com>
2020-03-10 12:17:59 +0300

    minor fixes

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,11 +1,37 @@
+let introHeight = document.querySelector("#intro").scrollHeight; // height of intro block 
 let feedbackModal = document.querySelector("#feedbackModal"); // feedback modal window
 let introBtn = document.querySelector("#introButton"); // feedback modal window button
-let worksModal = document.querySelector("#worksModal"); // works feedback window
-let worksItem = document.querySelectorAll("#worksItem"); // works feedback window button
+let worksModal = document.querySelector("#worksModal"); // works modal window
+let worksItem = document.querySelectorAll("#worksItem"); 
+let aboutBtn = document.querySelector("#aboutBtn");
+let works = document.querySelector("#worksInner");
+let anchors = document.querySelectorAll('a[href*="#"]'); // anchors for sliding between blocks
+
+
+//=============== Smooth slide to anchor ============================//
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    let block = anchor.getAttribute('href').substr(1);
+    
+    document.getElementById(block).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+}
+
+//=============== Fixed menu ============================//
+
 
 // Feedback modal
 // Open
 introBtn.onclick = () => {
+  feedbackModal.style.display = "flex";
+}
+
+aboutBtn.onclick = () => {
   feedbackModal.style.display = "flex";
 }
 
@@ -28,11 +54,28 @@ window.onclick = function(event) {
 }
 
 
-// Slider Jquery
-$(document).ready(function() {
-  $('.instruments__slider').slick({
-    autoplay: true,
-    autoplaySpeed: 4000,
-    dots: true
+
+for (i = 0; i < worksItem.length; i++) {
+  let item = worksItem[i];
+  item.addEventListener("mouseover", function() {
+    let drop = document.createElement("div");
+    let title = document.createElement("div");
+
+    drop.classList.add("works__drop");
+    title.classList.add("drop__title");
+
+    drop.id = "drop";
+    title.id = "dropTitle";
+
+    drop.appendChild(title);
+    item.appendChild(drop);
+
+    works.style.marginBottom = "60px";
   });
-});
+  item.addEventListener("mouseout", function() {
+    let drop = document.getElementById("drop");
+
+    item.removeChild(drop);
+    works.style.marginBottom = "100px";
+  });
+}
---
+++ dist/assets/js/slider.js
@@ -0,0 +1,8 @@
+// Slider Jquery
+$(document).ready(function() {
+    $('.instruments__slider').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true
+    });
+  });
